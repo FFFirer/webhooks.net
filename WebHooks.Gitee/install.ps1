5768b993d546e6d5f7580131c4ebefb7efc17d72
fffirer <0sdf0@sina.cn>
2021-11-28 23:03:27 +0800

    [Update] 更新安装脚本

--- WebHooks.Gitee/install.ps1
+++ WebHooks.Gitee/install.ps1
@@ -1,3 +1,6 @@
+$VerbosePreference = "Continue"
+$DebugPreference = "Continue"
+
 $InstallPath = "/var/www/webhooks-gitee"
 
 # 首先编译发布项目
@@ -5,29 +8,30 @@ try {
     dotnet publish "./WebHooks.Gitee.csproj" -c Release -o publish
 }
 catch {
-    Write-Output "发布失败，$_"
+    Write-Warning "发布失败，$_"
     exit 1
 }
 
 $ExistsInstallPath = Test-Path $InstallPath
 
 if (-not $ExistsInstallPath) {
-    Write-Output "没有找到安装目录：$InstallPath"
+    Write-Warning "没有找到安装目录：$InstallPath"
+    exit 1
 }
 
-Copy-Item -Path "./publish" -Destination $InstallPath -Recurse
+Copy-Item -Path "./publish" -Destination $InstallPath -Recurse -ErrorAction Stop
 
-Write-Output "开始修改权限"
+Write-Debug "开始修改权限"
 chown -R www-data:www-data $InstallPath
-Write-Output "修改 $InstallPath 所有者为 www-data:www-data"
+Write-Debug "修改 $InstallPath 所有者为 www-data:www-data"
 
 # 服务安装文件
 $InstallServicePath = "./webhooks.gitee.service"
 
-Write-Output "复制service文件到systemd路径[/etc/systemd/system/]下"
+Write-Debug "复制service文件到systemd路径[/etc/systemd/system/]下"
 Copy-Item $InstallServicePath -Destination "/etc/systemd/system/" -ErrorAction Stop
 
-Write-Output "文件复制完成"
+Write-Debug "文件复制完成"
 exit 0
 
 
