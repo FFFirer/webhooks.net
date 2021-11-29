8208c46b71a8eaa251af0b6f77abe920f03dddd1
fffirer <0sdf0@sina.cn>
2021-11-30 03:24:34 +0800

    [Update] 更新安装脚本

--- WebHooks.Gitee/install.ps1
+++ WebHooks.Gitee/install.ps1
@@ -4,13 +4,7 @@ $DebugPreference = "Continue"
 $InstallPath = "/var/www/webhooks-gitee"
 
 # 首先编译发布项目
-try {
-    dotnet publish "./WebHooks.Gitee.csproj" -c Release -o publish
-}
-catch {
-    Write-Warning "发布失败，$_"
-    exit 1
-}
+
 
 $ExistsInstallPath = Test-Path $InstallPath
 
@@ -20,21 +14,54 @@ if (-not $ExistsInstallPath) {
     New-Item -Path $InstallPath -ItemType Directory
 
     Write-Debug "新建文件夹：$InstallPath"
+
+    Write-Debug "开始修改权限"
+    chown -R www-data:www-data $InstallPath
+    Write-Debug "修改 $InstallPath 所有者为 www-data:www-data"
 }
 
-Copy-Item -Path "./publish/*" -Destination $InstallPath -Force -Recurse -ErrorAction Stop
-
-Write-Debug "开始修改权限"
-chown -R www-data:www-data $InstallPath
-Write-Debug "修改 $InstallPath 所有者为 www-data:www-data"
-
 # 服务安装文件
-$InstallServicePath = "./webhooks.gitee.service"
 
-Write-Debug "复制service文件到systemd路径[/etc/systemd/system/]下"
-Copy-Item $InstallServicePath -Destination "/etc/systemd/system/" -ErrorAction Stop
+$InstallServiceFolder = "/etc/systemd/system"
+$InstallServiceFile = "webhooks.gitee.service"
 
+$InstallServiceTargetPath = Join-Path -Path $InstallServiceFolder -ChildPath $InstallServiceFile
+
+Write-Debug "systemd配置文件路径: $InstallServiceTargetPath"
+
+$ExistsServiceFile = Test-Path $InstallServiceTargetPath
+
+if (-not $ExistsServiceFile) {
+    Write-Debug "没有发现systemd配置文件"
+    Write-Debug "复制service文件到systemd路径[/etc/systemd/system/]下"
+
+    Copy-Item $InstallServiceFile -Destination "/etc/systemd/system/" -ErrorAction Stop
+}
+else {
+    Write-Debug "已存在systemd配置文件"
+
+    Write-Debug "停止systemd服务 $InstallServiceFile"
+    systemctl stop $InstallServiceFile
+}
+
+# 拷贝文件
+try {
+    Write-Debug "开始发布"
+    dotnet publish "./WebHooks.Gitee.csproj" -c Release -o publish
+}
+catch {
+    Write-Warning "发布失败，$_"
+    exit 1
+}
+
+Write-Debug "更新文件"
+Copy-Item -Path "./publish/*" -Destination $InstallPath -Force -Recurse -Exclude "./publish/appsetting.*.json", "./publish/webhooks.json" -ErrorAction Stop
 Write-Debug "文件复制完成"
+
+Write-Debug "启动systemd服务 $InstallServiceFile"
+systemctl start $InstallServiceFile
+systemctl status $InstallServiceFile
+
 exit 0
 
 
