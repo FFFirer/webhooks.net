ebfabdd0d37eedbe3fd8fb85ed0279f43a7e77ea
fffirer <0sdf0@sina.cn>
2022-05-04 03:36:17 +0800

    [update] 模态框代码优化

--- webhooks-ui/src/components/Shared/BsModal/BsModalHelper.ts
+++ webhooks-ui/src/components/Shared/BsModal/BsModalHelper.ts
@@ -3,7 +3,7 @@ import { Modal } from "bootstrap";
 function useModal(target: string): Modal {
     const el = document.getElementById(target);
     if (!el) {
-        return null;
+        throw "没有找到目标元素";
     }
     const modal = new Modal(el);
     return modal;
